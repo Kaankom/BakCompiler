e5995daeeeba8dce340654784656c7da72c16987
Kaan Komutekir <kaankom@hotmail.de>
2024-07-18 17:28:24 +0200

    chore: added helpers

--- src/compiler.h
+++ src/compiler.h
@@ -3,27 +3,23 @@
 
 #include <stdio.h>
 
-enum
-{
-  COMPILER_FILE_COMPILED_OK,
-  COMPILER_FAILED_WITH_ERRORS
-};
+enum { COMPILER_FILE_COMPILED_OK, COMPILER_FAILED_WITH_ERRORS };
 
-struct compile_process
-{
+struct compile_process {
   // Compiling Options
   int flags;
 
-  struct compile_process_input_file
-  {
-    FILE* fp;
-    const char* abs_path;
+  struct compile_process_input_file {
+    FILE *fp;
+    const char *abs_path;
   } cfile;
 
-  FILE* ofile;
+  FILE *ofile;
 };
 
-int compile_file(const char* filename, const char* out_filename, int flags);
-struct compile_process* compile_process_create(const char* filename, const char* filename_out, int flags);
+int compile_file(const char *filename, const char *out_filename, int flags);
+struct compile_process *compile_process_create(const char *filename,
+                                               const char *filename_out,
+                                               int flags);
 
 #endif
